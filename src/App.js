47d1ccba39dd55dc3e8231d6e5de30e0948e6f07
mohits Projects <mohit5150515@gmail.com>
2021-03-19 22:46:54 +0530

    creating elements using rough canvas

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
-import "./App.css";
-import React, { useLayoutEffect, useState } from "react";
+import React, { createElement, useLayoutEffect, useState } from "react";
 import rough from "roughjs/bundled/rough.esm";
 const generator = rough.generator();
 
-function App() {
+const createElement = (x1, y1, x2, y2) => {
+  const roughElement = generator.line(x1, y1, x2, y2);
+  return { x1, y1, x2, y2, roughElement };
+};
+
+const App = () => {
   const [elements, setElements] = useState([]);
   const [drawing, setDrawing] = useState(false);
-
-  const handleMouseDown = () => {};
-  const handleMouseMove = () => {};
-  const handleMouseUp = () => {};
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    const roughCanvas = rough.canvas(canvas);
-    const rect = generator.rectangle(10, 10, 100, 100);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    roughCanvas.draw(rect);
+    const roughCanvas = rough.canvas(canvas);
+
+    elements.forEach(({ roughElement }) => roughCanvas.draw(roughElement));
+    // const rect = generator.rectangle(10, 10, 100, 100);
+
+    // roughCanvas.draw(rect);
 
     // ctx.fillStyle = "green";
     // ctx.fillRect(10, 10, 150, 100);
-  });
+  }, [elements]);
+
+  const handleMouseDown = (e) => {
+    setDrawing(true);
+    const { clientX, clientY } = e;
+    // now we need to create elements
+    const element = createElement(clientX, clientY, clientX, clientY);
+    setElements((prevState) => [...prevState, element]);
+  };
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+    const { clientX, clientY } = event;
+    const index = elements.length - 1;
+    const { x1, y1 } = elements[index];
+    const updatedElement = createElement(x1, y1, clientX, clientY);
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement;
+    setElements(elementsCopy);
+  };
+  const handleMouseUp = (e) => {
+    setDrawing(false);
+  };
+
   return (
     <canvas
       id="canvas"
@@ -35,6 +61,6 @@ function App() {
       canvas
     </canvas>
   );
-}
+};
 
 export default App;
