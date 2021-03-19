1ff43a0bd53da8e1408805b4a946e5a5745f50f3
mohits Projects <mohit5150515@gmail.com>
2021-03-19 18:20:30 +0530

    adding basic canvas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import React, { useLayoutEffect, useState } from "react";
 
 function App() {
+  useLayoutEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(10, 10, 150, 100);
+  });
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+    <canvas id="canvas" height={window.innerHeight} width={window.innerWidth}>
+      canvas
+    </canvas>
   );
 }
 
