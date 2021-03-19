7ad290b5bd51115cf1f1a613a136590efbeffd53
mohits Projects <mohit5150515@gmail.com>
2021-03-19 18:46:48 +0530

    added basic rough canvas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import "./App.css";
 import React, { useLayoutEffect, useState } from "react";
+import rough from "roughjs/bundled/rough.esm";
+const generator = rough.generator();
 
 function App() {
+  const [elements, setElements] = useState([]);
+  const [drawing, setDrawing] = useState(false);
+
+  const handleMouseDown = () => {};
+  const handleMouseMove = () => {};
+  const handleMouseUp = () => {};
+
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
 
-    ctx.fillStyle = "green";
-    ctx.fillRect(10, 10, 150, 100);
+    const roughCanvas = rough.canvas(canvas);
+    const rect = generator.rectangle(10, 10, 100, 100);
+
+    roughCanvas.draw(rect);
+
+    // ctx.fillStyle = "green";
+    // ctx.fillRect(10, 10, 150, 100);
   });
   return (
-    <canvas id="canvas" height={window.innerHeight} width={window.innerWidth}>
+    <canvas
+      id="canvas"
+      height={window.innerHeight}
+      width={window.innerWidth}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
       canvas
     </canvas>
   );
