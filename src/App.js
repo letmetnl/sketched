2958d599a5e963e5371eff276f71552a24030a70
mohits Projects <mohit5150515@gmail.com>
2021-03-20 19:39:15 +0530

    added basic movement on selecting element

--- src/App.js
+++ src/App.js
@@ -2,18 +2,78 @@ import React, { useLayoutEffect, useState } from "react";
 import rough from "roughjs/bundled/rough.esm";
 const generator = rough.generator();
 
-const creatingElement = (x1, y1, x2, y2, type) => {
+const creatingElement = (id, x1, y1, x2, y2, type) => {
   const roughElement =
     type === "line"
       ? generator.line(x1, y1, x2, y2)
       : generator.rectangle(x1, y1, x2 - x1, y2 - y1);
-  return { x1, y1, x2, y2, type, roughElement };
+  return { id, x1, y1, x2, y2, type, roughElement };
+};
+//function to whwther the coordinates match any existing element
+const isWithInElement = (x, y, element) => {
+  const { type, x1, x2, y1, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return x >= minX && x <= maxX && y >= minY && y <= maxY;
+  } else {
+    const a = { x: x1, y: y1 };
+    const b = { x: x2, y: y2 };
+    const c = { x, y };
+    //logic for checking whether the point is on line
+    const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+    const inside = Math.abs(offset) < 1 ? "inside" : null;
+  }
+};
+// calculating distance among two points
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const cursorForPosition = (position) => {
+  switch (position) {
+    case "tl":
+    case "br":
+    case "start":
+    case "end":
+      return "nwse-resize";
+    case "tr":
+    case "bl":
+      return "nesw-resize";
+    default:
+      return "move";
+  }
+};
+
+// function for adjusting coordinates
+const adjustElementCoordinates = (element) => {
+  const { type, x1, y1, x2, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  } else {
+    if (x1 < x2 || (x1 === x2 && y1 < y2)) {
+      return { x1, y1, x2, y2 };
+    } else {
+      return { x1: x2, y1: y2, x2: x1, y2: y1 };
+    }
+  }
+};
+
+// function for getting element for selection
+const getElementAtPosition = (x, y, elements) => {
+  return elements.find((element) => isWithInElement(x, y, element));
 };
 
 const App = () => {
   const [elements, setElements] = useState([]);
-  const [drawing, setDrawing] = useState(false);
+  const [action, setAction] = useState("none");
   const [tool, setTool] = useState("line");
+  const [selectedElement, setSelectedElement] = useState(null);
 
   useLayoutEffect(() => {
     const canvas = document.getElementById("canvas");
@@ -32,30 +92,89 @@ const App = () => {
     // ctx.fillRect(10, 10, 150, 100);
   }, [elements]);
 
+  const updateElement = (id, x1, y1, x2, y2, type) => {
+    const updatedElement = creatingElement(id, x1, y1, x2, y2, type);
+
+    const elementsCopy = [...elements];
+    elementsCopy[id] = updatedElement;
+    setElements(elementsCopy, true);
+  };
+
   const handleMouseDown = (e) => {
-    setDrawing(true);
     const { clientX, clientY } = e;
-    // now we need to create elements
-    const element = creatingElement(clientX, clientY, clientX, clientY, tool);
-    setElements((prevState) => [...prevState, element]);
+    // if we have choosen tool as select to select some existing element than
+    if (tool === "select") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({ ...element, offsetX, offsetY });
+        setAction("moving");
+      }
+    } else {
+      const id = elements.length;
+      setAction("drawing");
+
+      // now we need to create elements
+      const element = creatingElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool
+      );
+      setElements((prevState) => [...prevState, element]);
+    }
   };
   const handleMouseMove = (event) => {
-    if (!drawing) return;
     const { clientX, clientY } = event;
-    const index = elements.length - 1;
-    const { x1, y1 } = elements[index];
-    const updatedElement = creatingElement(x1, y1, clientX, clientY, tool);
-    const elementsCopy = [...elements];
-    elementsCopy[index] = updatedElement;
-    setElements(elementsCopy);
+
+    if (tool === "select") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      event.target.style.cursor = element
+        ? cursorForPosition(element.position)
+        : "default";
+    }
+    if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1 } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);
+      // const elementsCopy = [...elements];
+      // elementsCopy[index] = updatedElement;
+      // setElements(elementsCopy);
+    } else if (action === "moving") {
+      const { id, x1, x2, y1, y2, type, offsetX, offsetY } = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const newX1 = clientX - offsetX;
+      const newY1 = clientY - offsetY;
+      updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type);
+    }
   };
   const handleMouseUp = (e) => {
-    setDrawing(false);
+    if (selectedElement) {
+      const index = selectedElement.id;
+      const { id, type } = elements[index];
+      if (action === "drawing" || action === "resizing") {
+        const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
+        updateElement(id, x1, y1, x2, y2, type);
+      }
+    }
+    setAction("none");
+    setSelectedElement(null);
   };
 
   return (
     <div>
       <div style={{ position: "fixed" }}>
+        <input
+          type="radio"
+          id="select"
+          checked={tool === "select"}
+          onChange={() => setTool("select")}
+        />
+        <label htmlFor="select">Select</label>
         <input
           type="radio"
           id="line"
